3147fecde0cd08d72d7b90b4c5fffc2348c3b455
okiki123 <okikimalomo17@gmail.com>
2022-02-01 22:47:35 +0100

    Updated Commit

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
-
+import './footer.css'
 function Footer() {
-  return <div>this is footer</div>;
+  return <div className='footer'>Made by Malomo Okikioluwa</div>;
 }
 
 export default Footer;
